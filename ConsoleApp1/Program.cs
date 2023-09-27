f72b0136c4417c98e3271f65cb7be9c4bc57fc9f
VladSebov <sebov.vlad@inbox.ru>
2023-09-27 13:58:23 +0300

    Добавлен Калькулятор и .gitignore

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+    System.Console.WriteLine("Console Calculator");
+    System.Console.WriteLine("Possible operations: '+','-','*','/'");
+    System.Console.WriteLine("Enter the first number");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    System.Console.WriteLine("Enter the operation sign");
+    char operSign = Convert.ToChar(Console.ReadLine());
+    System.Console.WriteLine("Enter the second number");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+double result = 0;
+    if (operSign == '+')
+    {
+        result = num1 + num2;
+    }
+    if (operSign == '-')
+    {
+        result = num1 - num2;
+    }
+    if (operSign == '*')
+    {
+        result = num1 * num2;
+    }
+    if (operSign == '/')
+    {
+        result = num1 / num2;
+    }
+    Console.WriteLine("The result is " + result);
+    Console.ReadLine();
