d6b3a8605efd3975961c22f265421dc045fbd604
VladSebov <sebov.vlad@inbox.ru>
2023-10-11 14:08:26 +0300

    Закрыть программу ?

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,29 +1,35 @@
+string c;
 System.Console.WriteLine("Hello, User!");
 System.Console.WriteLine("This is Console Calculator");
 System.Console.WriteLine("Possible operations: '+','-','*','/'");
-System.Console.WriteLine("Enter the first number");
-double num1 = Convert.ToDouble(Console.ReadLine());
-System.Console.WriteLine("Enter the operation sign");
-char operSign = Convert.ToChar(Console.ReadLine());
-System.Console.WriteLine("Enter the second number");
-double num2 = Convert.ToDouble(Console.ReadLine());
-double result = 0;
-if (operSign == '+')
+do
 {
-    result = num1 + num2;
-}
-if (operSign == '-')
-{
-    result = num1 - num2;
-}
-if (operSign == '*')
-{
-    result = num1 * num2;
-}
-if (operSign == '/')
-{
-    if (num2 != 0) result = num1 / num2;
-}
-if (num2 == 0) Console.WriteLine("Division by zero error");
-else Console.WriteLine("The result is " + result);
-Console.ReadLine();
+    System.Console.WriteLine("Enter the first number");
+    double num1 = Convert.ToDouble(Console.ReadLine());
+    System.Console.WriteLine("Enter the operation sign");
+    char operSign = Convert.ToChar(Console.ReadLine());
+    System.Console.WriteLine("Enter the second number");
+    double num2 = Convert.ToDouble(Console.ReadLine());
+    double result = 0;
+    if (operSign == '+')
+    {
+        result = num1 + num2;
+    }
+    if (operSign == '-')
+    {
+        result = num1 - num2;
+    }
+    if (operSign == '*')
+    {
+        result = num1 * num2;
+    }
+    if (operSign == '/')
+    {
+        if (num2 != 0) result = num1 / num2;
+    }
+    if (num2 == 0) Console.WriteLine("Division by zero error");
+    else Console.WriteLine("The result is " + result);
+    System.Console.WriteLine("Quit Console Calculator (Y/N) ?");
+    c = Console.ReadLine();
+} while (c != "Y");
+
